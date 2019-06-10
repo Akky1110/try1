41fe99d43fcfd7ddb6d6c1d0439978b0bf268d1e
Akky1110 <akerke.tleugazy@nu.edu.kz>
2019-06-10 18:33:00 +0300

    Player is added

--- game.py
+++ game.py
@@ -9,13 +9,22 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.AMAZON)
         self.score = 0
 
+        self.all_sprites_list = arcade.SpriteList()
+        self.player_sprite = arcade.Sprite('character.png', 0.3)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.all_sprites_list.append(self.player_sprite)
+
+
     def on_draw(self):
         arcade.start_render()
         output = f'Score: {self.score: 02d}'
         arcade.draw_text(output, 100, 100, arcade.color.WHITE)
+        self.all_sprites_list.draw()
 
     def update(self, delta_time: float):
         self.score += 1
+        self.all_sprites_list.update()
 
 def main():
     MyGame(600, 600, 'My Game')
