a21a90d493edf8e015eedcbf2c7f1200cd5805bc
Akky1110 <akerke.tleugazy@nu.edu.kz>
2019-06-10 16:50:43 +0300

    Background color is amazon

--- game.py
+++ game.py
@@ -1,10 +1,17 @@
 import arcade as arcade
 import random
 
+
 class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.AMAZON)
+
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text('Hello', 100, 100, arcade.color.WHITE)
+
 
 def main():
     MyGame(600, 600, 'My Game')
